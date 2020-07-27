05d8f79391dbc06e825f3820a8ee2a88f00385b8
Giomar Rodriguez <giorodri0584@gmail.com>
2020-07-27 00:46:17 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,95 +1,144 @@
-import React, { useState } from 'react';
-import { firestore } from './firebase/firebase';
+import React, { useState } from "react";
+import { firestore } from "./firebase/firebase";
+import { useFormik } from "formik";
 
 const App = () => {
   const [submited, setSubmited] = useState(false);
-  const [article, setArticle] = useState({
-    title: '',
-    articleUrl: '',
-    coverImageUrl: '',
-    author: '',
-    authorLogo: '',
-    createdAt: Math.round((new Date()).getTime() / 1000)
-  }); 
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      articleUrl: "",
+      coverImageUrl: "",
+      author: "",
+      authorLogo: "",
+      createdAt: Math.round(new Date().getTime() / 1000),
+    },
+    onSubmit: values => saveArticle(values),
+    validate: values => validate(values)
+  });
 
-  const updateField = (e) => {
-    if(e.target.name === 'external'){
-      const val = e.target.value === 'true' ? true : false;
-      setArticle({
-        ...article,
-        [e.target.name]: val
-      });
-      return;
+  const validate = (values) => {
+    let errors = {};
+    if(!values.title){
+      errors.title = "Title is Required"
     }
-    setArticle({
-      ...article,
-      [e.target.name]: e.target.value
-    });
+    if(!values.articleUrl){
+      errors.articleUrl = "Article Url is Required"
+    }
+    if(!values.coverImageUrl){
+      errors.coverImageUrl = "Cover Image Url is Required"
+    }
+    if(!values.author){
+      errors.author = "Author is Required"
+    }
+    if(!values.authorLogo){
+      errors.authorLogo = "Author Logo  is Required"
+    }
+    return errors;
   }
 
-  const saveArticle = async () => {
-    firestore.collection("articles").add(article)
-      .then((docRef) => {
-        console.log("Article ID: " + docRef.id);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    
-  }
+  const saveArticle = async (values) => {
+    console.log(values);
+    // firestore
+    //   .collection("articles")
+    //   .add(article)
+    //   .then((docRef) => {
+    //     console.log("Article ID: " + docRef.id);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+  };
 
   return (
     <div className="container">
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <h4>News Article</h4>
         <div className="field">
           <label className="label">Title</label>
           <div className="control">
-            <input className="input" type="text" name="title" onChange={updateField} required></input>
+            <input
+              className="input"
+              type="text"
+              name="title"
+              onChange={formik.handleChange}
+              value={formik.values.title}
+              onBlur={formik.handleBlur}
+            ></input>
           </div>
+          {formik.touched.title && formik.errors.title ? <p className="help is-danger">{formik.errors.title}</p> : null}
         </div>
         <div className="field">
           <label className="label">Article Url</label>
           <div className="control">
-            <input className="input" type="text" name="articleUrl" onChange={updateField}></input>
+            <input
+              className="input"
+              type="text"
+              name="articleUrl"
+              onChange={formik.handleChange}
+              value={formik.values.articleUrl}
+              onBlur={formik.handleBlur}
+            ></input>
           </div>
+          {formik.errors.articleUrl && formik.touched.articleUrl ? <p className="help is-danger">{formik.errors.articleUrl}</p> : null}
         </div>
         <div className="field">
           <label className="label">Cover Image Url</label>
           <div className="control">
-            <input className="input" type="text" name="coverImageUrl" onChange={updateField}></input>
+            <input
+              className="input"
+              type="text"
+              name="coverImageUrl"
+              onChange={formik.handleChange}
+              value={formik.values.coverImageUrl}
+              onBlur={formik.handleBlur}
+            ></input>
           </div>
+          {formik.errors.coverImageUrl && formik.touched.coverImageUrl ? <p className="help is-danger">{formik.errors.coverImageUrl}</p> : null}
         </div>
         <div className="field">
-          <label className="label">Author</label>
+          <label className="label"> Author</label>
           <div className="control">
-            <div className="select">
-              <select name="author" onChange={updateField}>
-                <option>Select Author</option>
-                <option>Listin Diario</option>
-                <option>Despierta RD</option>
-              </select>
-            </div>
+            <input
+              className="input"
+              type="text"
+              name="author"
+              onChange={formik.handleChange}
+              value={formik.values.author}
+              onBlur={formik.handleBlur}
+            ></input>
           </div>
+          {formik.errors.author && formik.touched.author ? <p className="help is-danger">{formik.errors.author}</p> : null}
         </div>
         <div className="field">
           <label className="label">Author Logo URL</label>
           <div className="control">
-            <input className="input" type="text" name="authorLogo" onChange={updateField}></input>
+            <input
+              className="input"
+              type="text"
+              name="authorLogo"
+              onChange={formik.handleChange}
+              values={formik.values.authorLogo}
+              onBlur={formik.handleBlur}
+            ></input>
+          </div>
+          {formik.errors.authorLogo && formik.touched.authorLogo ? <p className="help is-danger">{formik.errors.authorLogo}</p> : null}
+        </div>
+        <br />
+        <div className="field is-group">
+          <div className="control">
+            <button className="button is-primary" type="submit" disabled={!(formik.isValid && formik.dirty)}>
+              Submit
+            </button>
           </div>
         </div>
       </form>
       <br />
-      <div className="field is-group">
-        <div className="control">
-          <button className="button" onClick={saveArticle} disabled={submited}>Submit</button>
-        </div>
-      </div>
       <div>
-        <pre>{ JSON.stringify(article, null, 2) }</pre>
+        <pre>{JSON.stringify(formik.values, null, 2)}</pre>
       </div>
     </div>
   );
-}
+};
 
 export default App;
